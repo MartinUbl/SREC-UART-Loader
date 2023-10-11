f5ad7cfdbd162b5d886db1ade0a446f06bd27b8c
Martin Ubl <ublm@kiv.zcu.cz>
2023-10-11 15:58:12 +0200

    Refactor the loader structure

--- main.cpp
+++ main.cpp
@@ -7,54 +7,55 @@
 #include <unistd.h>
 #endif
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #ifndef _WIN32
 using HANDLE = int;
 constexpr int INVALID_HANDLE_VALUE = -1;
 #endif
 
-#include <iostream>
-#include <fstream>
-#include <string>
-
-int main(int argc, char** argv)
-{
-    if (argc != 3)
-    {
-        std::cerr << "Usage:  uart_flasher.exe <filename> <port identifier>" << std::endl;
-        return 3;
-    }
-
-    std::string filename = argv[1];
-
-    HANDLE hComm;
-
+inline bool Is_Valid_Handle(HANDLE handle) {
 #ifdef _WIN32
-    hComm = CreateFileA(argv[2], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-#elif __APPLE__
-    hComm = open(argv[2], O_RDWR | O_NONBLOCK);
+    return handle != INVALID_HANDLE_VALUE;
 #else
-    hComm = open(argv[2], O_RDWR);
+    return handle > INVALID_HANDLE_VALUE;
 #endif
+}
 
-    if (hComm <= INVALID_HANDLE_VALUE)
-    {
-        std::cerr << "Error in opening serial port" << std::endl;
-        return 1;
-    }
+inline HANDLE Open_Port(const std::string& portSpec) {
+#ifdef _WIN32
+    return CreateFileA(portSpec.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+#elif __APPLE__
+    return open(portSpec.c_str(), O_RDWR | O_NONBLOCK);
+#else
+    return open(portSpec.c_str(), O_RDWR);
+#endif
+}
 
+inline void Close_Port(HANDLE handle) {
+#ifdef _WIN32
+    CloseHandle(handle);
+#else
+    close(handle);
+#endif
+}
+
+inline bool Set_Port_Parameters(HANDLE handle) {
 #ifdef _WIN32
     DCB serialParams;
 
-    GetCommState(hComm, &serialParams);
+    GetCommState(handle, &serialParams);
     serialParams.BaudRate = CBR_115200;
     serialParams.ByteSize = 8;
     serialParams.StopBits = ONESTOPBIT;
     serialParams.Parity = NOPARITY;
-    SetCommState(hComm, &serialParams);
+    SetCommState(handle, &serialParams);
 #else
     struct termios tty;
 
-    if (tcgetattr(hComm, &tty) != 0)
+    if (tcgetattr(handle, &tty) != 0)
     {
         std::cerr << "Error in configuring serial port (1)" << std::endl;
         return 2;
@@ -79,28 +80,67 @@ int main(int argc, char** argv)
     cfsetispeed(&tty, B115200);
     cfsetospeed(&tty, B115200);
 
-    if (tcsetattr(hComm, TCSANOW, &tty) != 0)
+    if (tcsetattr(handle, TCSANOW, &tty) != 0)
     {
         std::cerr << "Error in configuring serial port (2)" << std::endl;
-        return 2;
+        return false;
     }
 #endif
 
+    return true;
+}
+
+inline bool Write_Port(HANDLE handle, const std::string& input) {
+#ifdef _WIN32
+    unsigned long l;
+    return WriteFile(handle, input.c_str(), static_cast<DWORD>(input.length()), &l, NULL);
+#else
+    return write(handle, input.c_str(), static_cast<int>(input.length())) > 0;
+#endif
+}
+
+inline bool Read_Port_Char(HANDLE handle, char& target) {
+#ifdef _WIN32
+    unsigned long l = 1;
+    return ReadFile(handle, &target, 1, &l, NULL);
+#else
+    return (read(hComm, &c, 1) == 1);
+#endif
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage:  uart_flasher.exe <filename> <port identifier>" << std::endl;
+        return 3;
+    }
+
+    std::string filename = argv[1];
+
+    HANDLE hComm = Open_Port(argv[2]);
+
+    if (!Is_Valid_Handle(hComm)) {
+        std::cerr << "Error in opening serial port" << std::endl;
+        return 1;
+    }
+
+    if (!Set_Port_Parameters(hComm)) {
+        std::cerr << "Error in configuring serial port" << std::endl;
+        return 1;
+    }
+
     std::cout << "Opening serial port successful" << std::endl;
 
-    unsigned long l;
     std::string ln;
 
+    // ifstream scope
     {
         std::ifstream ifile(argv[1]);
         if (!ifile.is_open())
         {
             std::cerr << "Error in opening input file" << std::endl;
-#ifdef _WIN32
-            CloseHandle(hComm);
-#else
-            close(hComm);
-#endif
+            Close_Port(hComm);
             return 1;
         }
 
@@ -124,48 +164,27 @@ int main(int argc, char** argv)
         while (std::getline(ifile, ln))
         {
             linecounter++;
-#ifdef _WIN32
-            WriteFile(hComm, ln.c_str(), static_cast<DWORD>(ln.length()), &l, NULL);
-#else
-            write(hComm, ln.c_str(), static_cast<int>(ln.length()));
-#endif
+            Write_Port(hComm, ln);
 
             if (lineinc < ((100 * linecounter) / linecnt))
             {
                 lineinc = ((100 * linecounter) / linecnt);
                 std::cout << "Progress: " << lineinc << " %" << std::endl;
             }
-
-            // Sleep(10);
-            sleep(1);
         }
     }
 
     std::cout << "Launching..." << std::endl;
 
-    ln = "G";
-#ifdef _WIN32
-    WriteFile(hComm, ln.c_str(), static_cast<DWORD>(ln.length()), &l, NULL);
-#else
-    write(hComm, ln.c_str(), static_cast<int>(ln.length()));
-#endif
+    Write_Port(hComm, "G");
 
     std::cout << "Done. Switching to listener mode." << std::endl << std::endl;
 
     char c;
-
-#ifdef _WIN32
-    while (ReadFile(hComm, &c, 1, &l, NULL))
-#else
-    while (read(hComm, &c, 1) == 1)
-#endif
+    while (Read_Port_Char(hComm, c))
         std::cout << c;
 
-#ifdef _WIN32
-    CloseHandle(hComm);
-#else
-    close(hComm);
-#endif
+    Close_Port(hComm);
 
     return 0;
 }
